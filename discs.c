6da796f8de027a32b14ff5030d24d050ab954bf3
wasilewskiJ <wasilewski.jakub@outlook.com>
2024-04-12 18:06:21 +0200

    o(n^2) solution

--- discs.c
+++ discs.c
@@ -5,12 +5,12 @@
 #include <string.h>
 
 struct disc {
-    int left;
-    int right;
+    long int left;
+    long int right;
 };
 
 void initializeDiscs(struct disc discs[], int N, int A[]) {
-    for (int i = 0; i < N; i++) {
+    for (long int i = 0; i < N; i++) {
         discs[i].left = i - A[i];
         discs[i].right = i + A[i];
     }
@@ -44,10 +44,10 @@ void mergeSort(struct disc discs[], int start, int end) {
 void merge(struct disc *discs, int start, int middle, int end) {
     int leftSize = middle - start + 1;
     int rightSize = end - middle;
-    printf("PRZED SORTOWANIEM: \n");
+    //printf("PRZED SORTOWANIEM: \n");
     struct disc leftDiscs[leftSize];
     struct disc rightDiscs[rightSize];
-    printDiscsArrayPoints(discs, start, end);
+    //printDiscsArrayPoints(discs, start, end);
     memcpy(leftDiscs, discs + start, leftSize * sizeof(struct disc));
     memcpy(rightDiscs, discs + middle + 1, rightSize * sizeof(struct disc));
     int l = 0, r = 0, a = start;
@@ -68,8 +68,8 @@ void merge(struct disc *discs, int start, int middle, int end) {
         memcpy(discs+a, rightDiscs+r, sizeof(struct disc) * (rightSize - r));
     }
 
-    printf("PO SORTOWANIU: \n");
-    printDiscsArrayPoints(discs, start, end);
+    //printf("PO SORTOWANIU: \n");
+    //printDiscsArrayPoints(discs, start, end);
     //printf("pierwsze czytanie\n");
     //printDiscsArray(discs, N);
 }
@@ -80,16 +80,25 @@ void printDiscsArray(struct disc * discs, int N) {
     }
 }
 
-int solution(int A[], int N) {
-    struct disc discs[4] ={{4, 2}, {3, 2}, {2,2}, {1,2}};
-    mergeSort(discs, 0, 3);
-    printDiscsArray(discs, 3);
-    return 0;
+long int solution(int A[], int N) {
+    struct disc discs[N];
+    initializeDiscs(discs, N, A);
+    mergeSort(discs, 0, N-1);
+    //printDiscsArray(discs, N-1);
+    long int total_pairs = 0;
+    for (int i = 0; i < N; i++) {
+        for (int j = N-1; i < j; j--) {
+            if(discs[i].right >= discs[j].left) {
+                total_pairs += j-i;
+                break; 
+            }
+        }
+    }
+    return total_pairs;
 }
 
 int main() {
-    struct disc discs[4] = {{4, 2}, {3, 2}, {2,2}, {1,2}};
-    mergeSort(discs, 0,3);
-    printDiscsArray(discs, 4);
+    int A[3] = {1,1,7};
+    printf("%d",solution(A, 3));
     return 0;
 }
